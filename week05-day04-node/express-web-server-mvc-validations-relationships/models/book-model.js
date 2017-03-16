f0922ff253cfa563cef2def47eadb126dd607b93
Tola Olaoke <tolaoke@spartaglobal.co>
2017-03-16 15:30:51 +0000

    add book model

---
+++ week05-day04-node/express-web-server-mvc-validations-relationships/models/book-model.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var Book = mongoose.model('Book', {
+  title: {
+    type: String,
+    required: true
+  },
+
+  author: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = Book;
---
+++ week05-day04-node/express-web-server-mvc-validations-relationships/models/user-model.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User', {
+  firstName: {
+    type: String,
+    required: true
+  },
+
+  lastName: String,
+
+  email: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = User;
