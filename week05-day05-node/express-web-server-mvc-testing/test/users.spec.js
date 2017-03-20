50a4cfd0ed9867934df1ecaba35fd1b9092b89dc
Tola Olaoke <tolaoke@spartaglobal.co>
2017-03-20 13:42:11 +0000

    finalised tests

--- week05-day05-node/express-web-server-mvc-testing/test/users.spec.js
+++ week05-day05-node/express-web-server-mvc-testing/test/users.spec.js
@@ -20,12 +20,14 @@ function getFirstUserIdFromUserListHTML(html) {
   return pathElements[2];
 }
 
-describe('Users', function () {
   // before running any tests ... before we run any of these 'it' functions we are going to run these anonymous functions
+describe('Users', function () {
   beforeEach(function () {
     request = chai.request(app);
   });
 
+
+// TEST FOR NEW---------------------------------------------------------------
   describe('GET', function () {
     it('should return error for invalid URL GET', function (done) {
       request
@@ -48,37 +50,94 @@ describe('Users', function () {
     });
   });
 
+
+// TEST FOR UPDATE -------------------------------------------------------------
+  console.log('Update Testing');
+
   describe('PUT', function () {
     it('should return error for non-existent user id', function (done) {
       request
-        .put('/users/non-existent-user-id')
-        .end(function (err, res) {
-          res.should.have.status(404);
-          done();
-        });
+      .put('/users/non-existent-user-id')
+      .end(function (err, res) {
+        res.should.have.status(404);
+        done();
+      });
     });
     it('should return correct result for existing user', function (done) {
       request
-        .get('/users/')
-        .end(function (err, res) {
-          var userId = getFirstUserIdFromUserListHTML(res.text);
+      .get('/users/')
+      .end(function (err, res) {
+        var userId = getFirstUserIdFromUserListHTML(res.text);
 
-          request
-            .put('/users/' + userId)
-            // setting a header
-            .set('content-type', 'application/x-www-form-urlencoded')
+        request
+          .put('/users/' + userId)
+              // setting a header
+          .set('content-type', 'application/x-www-form-urlencoded')
             // sending a package data with this 'put' request
-            .send({'firstName': 'testFirstName', 'lastName': 'testLastName', 'email': 'testEmail'})
-            .end(function (err, res) {
-              res.should.have.status(200);
-              res.text.should.match(/testFirstName/);
-              res.text.should.match(/testLastName/);
-              done();
-            });
-        });
+          .send({'firstName': 'testFirstName', 'lastName': 'testLastName', 'email': 'testEmail'})
+          .end(function (err, res) {
+            res.should.have.status(200);
+            res.text.should.match(/testFirstName/);
+            res.text.should.match(/testLastName/);
+            done();
+          });
+      });
     });
   });
 
+
+ // TEST FOR CREATE USER--------------------------------------------------------
+  console.log('Create Testing');
+  describe('POST', function () {
+    it('should return error when firstName is blank', function (done) {
+      request
+     .post('/users')
+     .set('Content-Type', 'application/x-www-form-urlencoded')
+     .send({ firstName: '', email: 'testpostlastname@example.com' })
+     .end(function (err, res) {
+       var jsonResponse = JSON.parse(res.text);
+
+       res.should.have.status(400);
+       expect(jsonResponse).to.be.an('array');
+       expect(jsonResponse.length).to.equal(1);
+       expect(jsonResponse[0].path).to.equal('firstName');
+       done();
+     });
+    });
+    it('should return error email is blank', function (done) {
+      request
+     .post('/users')
+     .set('Content-Type', 'application/x-www-form-urlencoded')
+     .send({ firstName: 'testPostFirstName', email: '' })
+     .end(function (err, res) {
+       var jsonResponse = JSON.parse(res.text);
+
+       res.should.have.status(400);
+       expect(jsonResponse).to.be.an('array');
+       expect(jsonResponse.length).to.equal(1);
+       expect(jsonResponse[0].path).to.equal('email');
+       done();
+     });
+    });
+    it('should create new user when data is valid', function (done){
+      request
+      .post('/users')
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .send({ firstName: 'testFirstName', email: 'testpost@example.com' })
+       .end(function (err, res) {
+         var firstNameRegExp = new RegExp(testFirstName);
+
+         res.should.have.status(200);
+         console.log('res.text', res.text);
+         res.text.should.match(firstNameRegExp);
+         done();
+       });
+    });
+  });
+
+
+
+//  TEST FOR DELETE USER ------------------------------------------------------
   describe('DELETE', function () {
     it('should return error for non-existent user id', function (done) {
       request
@@ -116,32 +175,3 @@ describe('Users', function () {
 // - like this:
 // ```.set('content-type', 'application/x-www-form-urlencoded') // set the form encoding type
 // .send({'title': 'Test Post', 'body': 'Body Text'})```
-
-
-
-// ----------------------------------------------------------
-
-// describe('UPDATE', function () {
-//   it('should return error for non-existent user id', function (done) {
-//     request
-//         .put('/users/:id/non-existent-user-id')
-//         .end(function (err, res) {
-//           res.should.have.status(404);
-//           done();
-//         });
-//   });
-//   it('should return updated result for existing user', function (done) {
-//     request
-//         .put('/users/:id')
-//         .end(function (err, res) {
-//           var userId = getFirstUserIdFromUserListHTML(res.text);
-//
-//           request
-//             .delete('/users/:id' + userId)
-//             .end(function (err, res) {
-//               res.should.have.status(200);
-//               done();
-//             });
-//         });
-//   });
-// });
