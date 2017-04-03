ebc82b9624ae98a0334a8e1adcde87b712773a67
Tola Olaoke <tolaoke@spartaglobal.co>
2017-04-03 12:34:06 +0100

    add use of link() function in directive

--- week08/week08-day01-angular-directives/angular-directives/directives/profile/profile.directive.js
+++ week08/week08-day01-angular-directives/angular-directives/directives/profile/profile.directive.js
@@ -8,6 +8,13 @@ angular
     scope: { //scope is an object, which is looking for an attribute with the following element
       person: '=' // = means two why binding
 
+    },
+    link:function(scope/*, element, attrs*/) { //this allows us to provide the view with a method  --element and attrs are other parameters that can be used.
+scope.showSkills = false; //sets the showSkills false so that is shows when the button assigned to it clicked
+
+      scope.toggleSkills = function (){
+        scope.showSkills = !scope.showSkills;
+      };
     }
 
   };
