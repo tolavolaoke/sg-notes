87e8502d0d0b613cb35e7f8a0b6660aa98e757c2
Tola Olaoke <tolaoke@spartaglobal.co>
2017-04-20 17:28:58 +0100

    add test for edit duck method

--- week10/week10-day04-angular-testing-with-services/angular-crud-with-ui-tests/frontend/spec/controllers/duck.controller.spec.js
+++ week10/week10-day04-angular-testing-with-services/angular-crud-with-ui-tests/frontend/spec/controllers/duck.controller.spec.js
@@ -10,7 +10,7 @@ describe('DuckController', () => {
     module('DuckApp'); // calls DuckApp from ng-controller
     inject(($controller, $httpBackend, _API_URL_) => {
       // creating an instance using the controllerToTest
-API_URL = _API_URL_
+      API_URL = _API_URL_;
       httpBackend = $httpBackend;
       mock$State = {
         go: jasmine.createSpy()
@@ -27,20 +27,27 @@ API_URL = _API_URL_
   });
 
   describe('initialisation', () => {
-    xit('Should do a basic test', () => {
-      console.log('got inside the basic test');
-      httpBackend.flush();
-    });
     it('Should populate allDucks with correct data', () => {
       const testDucks = ['duck one', 'duck two'];
 
+
       httpBackend
-        .expect('GET', `${API_URL}/ducks`)
-        .respond(testDucks);
+          .expect('GET', `${API_URL}/ducks`)
+          .respond(testDucks);
       httpBackend.flush();
       expect(controllerToTest.allDucks).toEqual(testDucks);
       console.log('controllerToTest.allDucks:', controllerToTest.allDucks);
 
     });
+  });
+
+  describe('editDuck()',() => {
+    it('Should go to "edit" state with specified duckId', () => {
+      const testDuckId = 'quark';
+
+      controllerToTest.editDuck(testDuckId);
+
+        expect(mock$State.go).toHaveBeenCalledWith('edit', { duckId: testDuckId });
+      });
   });
 });
